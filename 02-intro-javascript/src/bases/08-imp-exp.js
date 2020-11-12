49b1fc01bcd2b009fb858d3afbbd7e6f614a6a6f
hijuliancode <hijuliansosa@protonmail.com>
2020-11-11 19:55:32 -0500

    02: Import, export y funciones comunes de arreglos

---
+++ 02-intro-javascript/src/bases/08-imp-exp.js
@@ -0,0 +1,18 @@
+// Import, export y funciones comunes de arreglos
+
+import heroes, { owners } from '.. /data/heroes'
+
+const getHeroById = (id) => heroes.find( (hero) => hero.id === id )
+
+const getHeroesByOwner = (owner) => heroes.filter( (hero) => hero.owner === owner )
+
+// console.log( getHeroById(1) )
+// console.log( getHeroById(2) )
+// console.log( getHeroById(3) )
+// console.log( getHeroById(4) )
+// console.log( getHeroById(5) ) 
+
+console.log( getHeroesByOwner('DC') )
+
+console.log(owners)
+
