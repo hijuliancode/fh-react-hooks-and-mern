275cfcd4e421b55a03d34ebd195bd034c2f01fa9
hijuliancode <hijuliansosa@protonmail.com>
2020-11-11 23:55:15 -0500

    02: Operador Ternario

---
+++ 02-intro-javascript/src/bases/12-operador-ternario.js
@@ -0,0 +1,26 @@
+// Operador Condicional Ternario
+
+const activo = true;
+
+
+// Tradicional 
+  
+  // let mensaje = '';
+
+  // if ( !activo ) {
+  //   mensaje = 'Activo'
+  // } else {
+  //   mensaje = 'Inactivo'
+  // }
+
+// Operador Ternario
+
+  // const mensaje = ( activo ) ? 'Activo' : 'Inactivo'
+
+// Forma corta de hacer un If
+
+  const mensaje = !activo && 'Activo';
+
+console.log(mensaje)
+
+
