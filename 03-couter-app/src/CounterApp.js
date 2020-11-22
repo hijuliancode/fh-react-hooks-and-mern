a05778e039ee47cf057743b03a64c4a30bee8e66
hijuliancode <hijuliansosa@protonmail.com>
2020-11-22 11:19:29 -0500

    03: Evento click (Eventos en general)

--- 03-couter-app/src/CounterApp.js
+++ 03-couter-app/src/CounterApp.js
@@ -2,10 +2,19 @@ import React, { Fragment } from 'react';
 import PropTypes from 'prop-types'
 
 const CounterApp = ({ value }) => {
+
+    // handleAdd
+    const handleAdd = (e) => {
+        console.log(e)
+    }
+
     return (
         <>
             <h1>CounterApp</h1>
             <h2> { value } </h2>
+
+            {/* <button onClick={ (e) => handleAdd(e) } >+1</button> */}
+            <button onClick={ handleAdd } >+1</button>
         </>
     )
 }
