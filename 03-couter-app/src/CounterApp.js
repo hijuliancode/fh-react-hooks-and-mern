2721108ce10af48b0004b60010dc132ecad961c2
hijuliancode <hijuliansosa@protonmail.com>
2020-11-22 11:09:47 -0500

    03: Componente CounterApp

---
+++ 03-couter-app/src/CounterApp.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types'
+
+const CounterApp = ({ value }) => {
+    return (
+        <>
+            <h1>CounterApp</h1>
+            <h2> { value } </h2>
+        </>
+    )
+}
+
+CounterApp.propTypes = {
+    value: PropTypes.number.isRequired
+}
+
+export default CounterApp;
