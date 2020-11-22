3b54bd0ee91314746082291e0e769538fc3b690b
hijuliancode <hijuliansosa@protonmail.com>
2020-11-22 10:39:31 -0500

    03: Retornar elementos en el Componente - Fragment

--- 03-couter-app/src/PrimeraApp.js
+++ 03-couter-app/src/PrimeraApp.js
@@ -3,7 +3,12 @@ import React from 'react';
 // FC
 
 const PrimeraApp = () => {
-    return <h1>Hola Mundo</h1>;
+    return (
+        <>
+            <h1>Hola Mundo</h1>
+            <p>Mi primera aplicación</p>
+        </>
+    );
 }
 
 export default PrimeraApp;
