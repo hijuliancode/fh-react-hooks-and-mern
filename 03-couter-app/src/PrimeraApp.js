3cffae1103412139f1cbcb2d3d4c40070eb9c256
hijuliancode <hijuliansosa@protonmail.com>
2020-11-22 10:45:03 -0500

    03: Impresión de variables en el HTML

--- 03-couter-app/src/PrimeraApp.js
+++ 03-couter-app/src/PrimeraApp.js
@@ -3,9 +3,16 @@ import React from 'react';
 // FC
 
 const PrimeraApp = () => {
+    const saludo = 'Hola Mundo!'
+    const persona = {
+        nombre: 'Meraki',
+        edad: 7
+    }
     return (
         <>
-            <h1>Hola Mundo</h1>
+            {/* <pre>{ JSON.stringify(persona, null, 2) }</pre> */}
+            {/* <hr/> */}
+            <h1> {saludo} </h1>
             <p>Mi primera aplicación</p>
         </>
     );
