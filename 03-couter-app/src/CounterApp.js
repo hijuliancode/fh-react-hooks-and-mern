5743a8b2ab5c5b32f9b0dbc47cb79d57dbb6741b
hijuliancode <hijuliansosa@protonmail.com>
2020-11-26 21:09:11 -0500

    03: useState - Hook

--- 03-couter-app/src/CounterApp.js
+++ 03-couter-app/src/CounterApp.js
@@ -1,17 +1,20 @@
-import React, { Fragment } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types'
 
 const CounterApp = ({ value }) => {
 
+    const [ counter, setCounter] = useState(0)
+
     // handleAdd
     const handleAdd = (e) => {
-        console.log(e)
+        // setCounter(counter + 1)
+        setCounter( (c) => c + 1 )
     }
 
     return (
         <>
             <h1>CounterApp</h1>
-            <h2> { value } </h2>
+            <h2> { counter } </h2>
 
             {/* <button onClick={ (e) => handleAdd(e) } >+1</button> */}
             <button onClick={ handleAdd } >+1</button>
