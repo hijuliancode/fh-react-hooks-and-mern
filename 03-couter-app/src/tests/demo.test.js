19748157ffdac62537ec897203c86b1a7bf917f8
hijuliancode <hijuliansosa@protonmail.com>
2020-12-20 23:22:59 -0500

    04: Pruebas: Jest - Expect - toBe

--- 03-couter-app/src/tests/demo.test.js
+++ 03-couter-app/src/tests/demo.test.js
@@ -1,7 +1,20 @@
 
-test( 'Mi primera prueba, debe informar que es lo que estamos probando', () => {
-    const isActive = true;
-    if ( isActive ) {
-        throw new Error('No está activo')
-    }
-} )
+// test( 'Mi primera prueba, debe informar que es lo que estamos probando', () => {
+//   const isActive = true;
+//   if ( isActive ) {
+//     throw new Error('No está activo')
+//   }
+// })
+
+describe('Agrupar pruebas: Pruebas en el archivo demo.test.js', () => {
+  test('deben de ser iguales los string', () => {
+    // 1. Inicialización: Preparación de lo que queremos hacer
+    const mensaje = 'hola mundo'
+  
+    // 2. Estímulo
+    const mensaje2 = `hola mundo`
+  
+    // 3. Observar el comportamiento
+    expect( mensaje ).toBe( mensaje2 ) // ===
+  })  
+})
