2a1e632e254415fa45ab9d9c03023578ca2e6caa
hijuliancode <hijuliansosa@protonmail.com>
2020-12-26 12:52:49 -0500

    04: Pruebas: Enzyme - Testing unit

--- 03-couter-app/src/tests/PrimeraApp.test.js
+++ 03-couter-app/src/tests/PrimeraApp.test.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import { render } from "@testing-library/react"
+import { shallow } from 'enzyme';
+// import { render } from "@testing-library/react"
+
 import PrimeraApp from "../PrimeraApp"
 
 describe('Pruebas en PrimeraApp', () => {
-  test('should mostrar el mensaje Hola, soy Julian', () => {
-    const saludo = 'Hola, soy Julian'
-    // const wrapper = render( <PrimeraApp saludo={saludo} /> )
-    const { getByText } = render( <PrimeraApp saludo={saludo} /> )
-    expect( getByText(saludo) ).toBeInTheDocument()
+  // test('should mostrar el mensaje Hola, soy Julian', () => {
+  //   const saludo = 'Hola, soy Julian'
+  //   // const wrapper = render( <PrimeraApp saludo={saludo} /> )
+  //   const { getByText } = render( <PrimeraApp saludo={saludo} /> )
+  //   expect( getByText(saludo) ).toBeInTheDocument()
+  // })
+  test('should mostrar <PrimeraApp/> correctamente', () => {
+    const saludo = 'Hola, soy Goku'
+    const wrapper = shallow( <PrimeraApp saludo={ saludo } /> )
 
+    expect( wrapper ).toMatchSnapshot();
   })
+  
 })
