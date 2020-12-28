879e0c3c630280986661e5a078585abea93e2e22
hijuliancode <hijuliansosa@protonmail.com>
2020-12-27 19:01:51 -0500

    GifExpertApp: GifExpertApp - Component

---
+++ 04-gif-expert-app/src/components/GifExpertApp/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const GifExpertAppComponent = () => {
+  return ( <>
+    <div className="container">
+      <h2 class="is-size-5-mobile is-size-4 has-text-centered mt-4">Gif Expert App</h2>
+      <hr />
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id sunt ducimus sapiente similique necessitatibus blanditiis tempora quisquam doloribus totam at quis exercitationem eum eligendi, vero rem pariatur, dicta nam sit.</p>
+    </div>
+  </> );
+}
+ 
+export default GifExpertAppComponent;
--- 04-gif-expert-app/src/index.js
+++ 04-gif-expert-app/src/index.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import GifExpertAppComponent from "./components/GifExpertApp";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <GifExpertAppComponent />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
