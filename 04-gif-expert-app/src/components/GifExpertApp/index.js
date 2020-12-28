cfab45e9e222b3fb4ebb41affb0c7c3379c6e1f9
hijuliancode <hijuliansosa@protonmail.com>
2020-12-27 19:49:52 -0500

    GifExpertApp: Creando una lista de categorias, randomId, input Search, useState

--- 04-gif-expert-app/src/components/GifExpertApp/index.js
+++ 04-gif-expert-app/src/components/GifExpertApp/index.js
@@ -1,13 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const GifExpertAppComponent = () => {
-  return ( <>
+  const [ categories, setCategories ] = useState([
+    { id: 'OnePunch', name: 'One Punch'},
+    { id: 'SamuraiX', name: 'Samurai X'},
+    { id: 'DragonBall', name: 'Dragon Ball'},
+  ])
+
+  const handleAdd = () => {
+    let inputSearch = document.getElementById('searchCategory')
+    let inputSearchValue = inputSearch.value
+
+    if ( inputSearchValue ) {
+      let newCategorie = {
+        id: guidGenerator(),
+        name: inputSearchValue
+      }
+      setCategories( cats => [...cats, newCategorie] )
+
+      // Reset Input
+      inputSearch.value = ''
+      inputSearch.focus()
+    }
+  }
+
+  function guidGenerator() { // Globally Unique IDentifier
+    var S4 = function() {
+       return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+    };
+    return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
+}
+
+  return (
     <div className="container">
-      <h2 class="is-size-5-mobile is-size-4 has-text-centered mt-4">Gif Expert App</h2>
+      <h2 className="is-size-5-mobile is-size-4 has-text-centered mt-4">Gif Expert App</h2>
       <hr />
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id sunt ducimus sapiente similique necessitatibus blanditiis tempora quisquam doloribus totam at quis exercitationem eum eligendi, vero rem pariatur, dicta nam sit.</p>
+
+      <div className="field has-addons mb-4 input-search">
+        <div className="control">
+          <input className="input" id="searchCategory" type="text" placeholder="Find a repository" />
+        </div>
+        <div className="control">
+          <button onClick={() => handleAdd()} className="button is-info" type="button">
+            Search
+          </button>
+        </div>
+      </div>
+
+      {
+        categories.map( (category) => {
+          return <button key={category.id} className="button is-small is-primary is-outlined mr-1 mb-1">{category.name}</button>
+        } )
+      }
+
     </div>
-  </> );
+  );
 }
  
 export default GifExpertAppComponent;
