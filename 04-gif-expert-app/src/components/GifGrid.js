3f5a990ede54ff2fd5af6c631d31b27cb4511dca
hijuliancode <hijuliansosa@protonmail.com>
2021-01-08 22:47:47 -0500

    GifExpertApp: Helpers - getGifs

--- 04-gif-expert-app/src/components/GifGrid.js
+++ 04-gif-expert-app/src/components/GifGrid.js
@@ -1,30 +1,16 @@
 import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 import { GifGridItem } from './GifGridItem'
+import { getGifs } from '../helpers/getGifs'
 
 export const GifGrid = ({ category }) => {
 
   const [ images, setImages ] = useState([])
 
   useEffect(() => {
-    getGifs()
-  }, [])
-
-  const getGifs = async () => {
-    const url = `https://api.giphy.com/v1/gifs/search?q=${category}&limit=10&api_key=8UAKyArOV82MyyYExfQcu8wNwm4G7SsI`;
-    const resp = await fetch(url);
-    const { data } = await resp.json();
-    const gifs = data.map( img => {
-      return {
-        id: img.id,
-        title: img.title,
-        url: img.images.downsized_medium.url,
-        externalUrl: `${img.url}/tile`,
-      }
-    })
-    console.log(gifs);
-    setImages(gifs)
-  }
+    getGifs(category)
+      .then( setImages )
+  }, [category])
 
   return (
     <>
