3824e8ac0a425216e41fd607f05035c87a71cb70
hijuliancode <hijuliansosa@protonmail.com>
2021-01-09 13:28:23 -0500

    GifExpertApp: Custom Hooks

--- 04-gif-expert-app/src/components/GifGrid.js
+++ 04-gif-expert-app/src/components/GifGrid.js
@@ -1,26 +1,36 @@
 import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
-import { GifGridItem } from './GifGridItem'
-import { getGifs } from '../helpers/getGifs'
+import { useFetchGifs } from '../hooks/useFetchGifs'
+// import { GifGridItem } from './GifGridItem'
+// import { getGifs } from '../helpers/getGifs'
 
 export const GifGrid = ({ category }) => {
 
-  const [ images, setImages ] = useState([])
+  // const [ images, setImages ] = useState([])
 
-  useEffect(() => {
-    getGifs(category)
-      .then( setImages )
-  }, [category])
+  const { loading } = useFetchGifs()
+
+  console.log(loading);
+
+  // useEffect(() => {
+  //   getGifs(category)
+  //     .then( setImages )
+  // }, [category])
 
   return (
     <>
-      {
+    <div className="column">
+      <h3>{ category }</h3>
+      { loading ? 'loading...' : 'Fin de carga' }
+    </div>
+
+      {/* {
         images.map( image => (
           <div key={ image.id } className="column is-one-quarter">
             <GifGridItem { ...image } />
           </div>
         ))
-      }
+      } */}
     </>
   )
 }
