a8ac107f860948312a9f4ecabf25bbbc29745a95
hijuliancode <hijuliansosa@protonmail.com>
2021-01-12 00:24:16 -0500

    GifExpertApp: Pruebas del componente - GifGridItem - Props, hasClass

--- 04-gif-expert-app/src/tests/components/GifGridItem.test.js
+++ 04-gif-expert-app/src/tests/components/GifGridItem.test.js
@@ -18,4 +18,32 @@ describe('Test GifGridItem.js', () => {
   test('Should render <GifGridItem />', () => {
     expect( wrapper ).toMatchSnapshot()
   })
+
+  test('Should as a <p> with title inside', () => {
+    const p = wrapper.find('p')
+    
+    expect( p.text().trim() ).toBe( dummyData.title )
+  })
+
+  test('Should render <img /> correctly', () => {
+    const img = wrapper.find('img')
+
+    // Puedo validar las propiedades de un elemento a traves de elm.props(), ejemplo img.props()
+    // También se puede validar solo una propiedad, ejemplo: img.prop('alt').toBe( dummyData.title )
+    // console.log(img.props())
+
+    expect( img.props().src ).toBe( dummyData.url )
+    expect( img.prop('src') ).toBe( dummyData.url ) // Lo mismo que el anterior
+
+    expect( img.props().alt ).toBe( dummyData.title )
+    expect( img.prop('alt') ).toBe( dummyData.title ) // Lo mismo que el anterior
+  })
+
+  test('Should has class animate__fadeIn', () => {
+    const className = wrapper.prop('className')
+    expect( className.includes('animate__fadeIn') ).toBe(true)
+
+    expect( wrapper.hasClass('animate__fadeIn') ).toBe( true ) // Forma nativa de Jest
+  })
+
 })
