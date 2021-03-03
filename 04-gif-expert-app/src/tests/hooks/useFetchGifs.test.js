ba5a0cdde7fcc83032aed68fb5f61723afdcf5a3
hijuliancode <hijuliansosa@protonmail.com>
2021-03-02 23:42:14 -0500

    GifExpertApp: customHook - waitForNextUpdate

--- 04-gif-expert-app/src/tests/hooks/useFetchGifs.test.js
+++ 04-gif-expert-app/src/tests/hooks/useFetchGifs.test.js
@@ -4,14 +4,26 @@ import { renderHook } from '@testing-library/react-hooks'
 import { useFetchGifs } from "../../hooks/useFetchGifs"
 
 describe('test in hook useFetchGifs', () => {
-  test('should return initial state', () => {
-    const category = 'Thor'
-    // const { data, loading } = useFetchGifs(category) // Debido a error generado en consola, no se hace de esta forma
 
-    const { result } = renderHook(() => useFetchGifs( category ))
-    const { data, loading } = result.current;
+  test('should return initial state', async () => {
+    // const { data, loading } = useFetchGifs('Thor') // Debido a error generado en consola, no se hace de esta forma
+    const { result, waitForNextUpdate } = renderHook(() => useFetchGifs( 'Thor' ))
+    const { data, loading } = result.current
+
+    await waitForNextUpdate()
 
     expect( data ).toEqual( [] )
     expect( loading ).toBe( true )
   })
+
+  test('should return images array and loading false', async () => {
+    const { result, waitForNextUpdate } = renderHook(() => useFetchGifs( 'Thor' ))
+    await waitForNextUpdate()
+
+    const { data, loading } = result.current
+
+    expect( data.length ).toEqual( 10 )
+    expect( loading ).toBe( false )
+  })
+
 })
