8dc14bbef0287ef981bbcecdb072e1ee165a4fe9
hijuliancode <hijuliansosa@protonmail.com>
2021-04-21 16:40:49 -0500

    HooksApp: useEffect : - Precauciones

--- 05-hook-app/src/components/02-useEffect/Message.js
+++ 05-hook-app/src/components/02-useEffect/Message.js
@@ -1,17 +1,31 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
-export const Message = () => {
+export const Message = (e) => {
+
+  const [ coords, setCoords ] = useState({ x: 0, y: 0 })
+
+  const { x, y } = coords
+
+  const mouseMove = (e) => {
+    setCoords({ x: e.x, y: e.y })
+  }
 
   useEffect(() => {
-    console.log('Componente montado')
+    // console.log('Componente montado')
+    window.addEventListener('mouseover', mouseMove)
+
     return () => {
-      console.log('Componente desmontado');
+      // console.log('Componente desmontado');
+      window.removeEventListener('mouseover', mouseMove)
     }
   })
 
   return (
     <div>
       <h1>Componente Mensaje</h1>
+      <p>
+        x: {x}, y: {y}
+      </p>
     </div>
   )
 }
