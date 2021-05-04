1741e04de75039e853faeac95b10a7cb8ba74b49
hijuliancode <hijuliansosa@protonmail.com>
2021-05-03 22:27:00 -0500

    HooksApp: useReducer - Creando el cascaron de la lista de TODOs

--- 05-hook-app/src/components/08-useReducer/TodoApp.js
+++ 05-hook-app/src/components/08-useReducer/TodoApp.js
@@ -17,8 +17,40 @@ export const TodoApp = () => {
 
   return (
     <div>
-      <h1>TodoApp</h1>
+      <h1>
+        TodoApp <small>({todos.length})</small>
+      </h1>
       <hr />
+      <div className="row">
+        <div className="col-7">
+          <ul className="list-group list-group-flush">
+            {todos.map((todo, i) => (
+              <li key={todo.id} className="list-group-item complete">
+                <p>
+                  {i + 1}. {todo.desc}
+                </p>
+                <button className="btn btn-danger">X</button>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="col-5">
+          <h4>Agregar todo</h4>
+          <hr />
+          <form>
+            <input
+              type="text"
+              name="description"
+              className="form-control"
+              placeholder="Aprender ..."
+              autocomplete="off"
+            />
+            <button className="btn btn-outline-primary mt-4 btn-block">
+              Agregar
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
