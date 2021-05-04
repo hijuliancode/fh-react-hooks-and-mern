18307bbfdc4133fc945d9b2d32e0bc961ce0b002
hijuliancode <hijuliansosa@protonmail.com>
2021-05-03 22:12:30 -0500

    HooksApp: useReducer - Todo List

---
+++ 05-hook-app/src/components/08-useReducer/TodoApp.js
@@ -0,0 +1,24 @@
+import React, { useReducer } from 'react';
+import './styles.css';
+import { todoReducer } from './todoReducer';
+
+const initialState = [
+  {
+    id: new Date().getTime(),
+    desc: 'Aprender React',
+    done: false,
+  },
+];
+
+export const TodoApp = () => {
+  const [todos] = useReducer(todoReducer, initialState);
+
+  console.log(todos);
+
+  return (
+    <div>
+      <h1>TodoApp</h1>
+      <hr />
+    </div>
+  );
+};
