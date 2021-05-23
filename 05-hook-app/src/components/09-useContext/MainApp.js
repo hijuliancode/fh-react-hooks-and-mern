e2d20680080c0ed8bdfeaa8cc840d87c49ef27f3
hijuliancode <hijuliansosa@protonmail.com>
2021-05-23 14:39:57 -0500

    HooksApp: useContext: CreateContext y useContext

--- 05-hook-app/src/components/09-useContext/MainApp.js
+++ 05-hook-app/src/components/09-useContext/MainApp.js
@@ -1,8 +1,18 @@
 import React from 'react'
 import { AppRouter } from './AppRouter'
+import { UserContext } from './UserContext'
 
 export const MainApp = () => {
+
+  const user = {
+    id: '01777',
+    name: 'Julian Alejandro',
+    email: 'hijuliansosa@gmail.com'
+  }
+
   return (
-    <AppRouter />
+    <UserContext.Provider value={ user }>
+      <AppRouter />
+    </UserContext.Provider>
   )
 }
